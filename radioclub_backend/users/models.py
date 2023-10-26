7d5f3bbf537a886c07f2ce7a1f3bca755d2f156f
Tim Zubov <timofey.zubov.05@gmail.com>
2023-10-26 15:20:37 +0300

    Create simple user model. For now, it inherits from Abstractuser model (And therefore, inherits all default fields). I also add several new fields, such as ('is_banned', 'is_moderator', 'is_admin') that are self explanatory. The difference between moderator and admin is that admin can assign roles to other users.

--- radioclub_backend/users/models.py
+++ radioclub_backend/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        blank=False,
+        null=False
+    )
+    is_banned = models.BooleanField(
+        null=False,
+        default=False,
+    )
+    is_moderator = models.BooleanField(
+        null=False,
+        default=False,
+    )
+    is_admin = models.BooleanField(
+        null=False,
+        default=False,
+    )
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
